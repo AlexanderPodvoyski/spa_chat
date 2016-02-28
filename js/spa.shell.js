8c071ea4f531783e1acab5724ecdb8e86b75201c
AlexanderPodvoyski <alexander.podvoyski@upsilonit.com>
2016-02-28 20:09:43 +0300

    Add chat open/close functionality in spa.shell p.124

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -29,13 +29,23 @@ spa.shell = (function() {
         <div class="spa-shell-foot"></div>
         <div class="spa-shell-chat"></div>
         <div class="spa-shell-modal"></div>
-      `
+      `,
+      chat_extend_time: 1000,
+      chat_retract_time: 300,
+      chat_extend_height: 450,
+      chat_retract_height: 15,
+      chat_extended_title: 'Click to retract',
+      chat_retracted_title: 'Click to extended',
+      template_html: '<div class="spa-slider"><\/div>'
     };
   var
-    stateMap = { $$container : null },
+    stateMap = {
+      $container : null,
+      is_chat_retracted : true
+    },
     jqueryMap = {},
 
-    setJqueryMap, initModule;
+    setJqueryMap, toggleChat, initModule;
   //----------------- КОНЕЦ ПЕРЕМЕННЫХ В ОБЛАСТИ ВИДИМОСТИ МОДУЛЯ
 
   //----------------- НАЧАЛО СЛУЖЕБНЫХ МЕТОДОВ -------------------
@@ -44,11 +54,58 @@ spa.shell = (function() {
   //----------------- НАЧАЛО МЕТОДОВ DOM -------------------------
   setJqueryMap = function() {
     var $container = stateMap.$container;
-    jqueryMap = { $container: $container };
+    jqueryMap = {
+      $container: $container,
+      $chat: $container.find('.spa-shell-chat')
+    };
   };
+
+  toggleChat = function (do_extend, callback) {
+    var
+      px_chat_ht = jqueryMap.$chat.height(),
+      is_open = px_chat_ht === configMap.chat_extend_height,
+      is_closed = px_chat_ht === configMap.chat_retract_height,
+      is_sliding = ! is_open && ! is_closed;
+
+      if (is_sliding) return false;
+
+      if (do_extend) {
+        jqueryMap.$chat.animate(
+          { height: configMap.chat_extend_height },
+          configMap.chat_extend_time,
+          function() {
+            jqueryMap.$chat.attr(
+              'title', configMap.chat_extended_title
+            );
+            stateMap.is_chat_retracted = false;
+            if (callback){ callback(jqueryMap.$chat) }
+          }
+        );
+        return true;
+      }
+
+    jqueryMap.$chat.animate(
+      { height: configMap.chat_retract_height },
+      configMap.chat_retract_time,
+      function() {
+        jqueryMap.$chat.attr(
+          'title', configMap.chat_retracted_title
+        );
+        stateMap.is_chat_retracted = true;
+        if (callback){ callback(jqueryMap.$chat) }
+      }
+    );
+    return true;
+  };
+
+
   //----------------- КОНЕЦ МЕТОДОВ DOM --------------------------
 
   //----------------- НАЧАЛО ОБРАБОТЧИКОВ СОБЫТИЙ ----------------
+ onClickChat = function(event) {
+   toggleChat(stateMap.is_chat_retracted);
+   return false;
+ }
   //----------------- КОНЕЦ ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
 
   //----------------- НАЧАЛО ОТКРЫТЫХ МЕТОДОВ --------------------
@@ -56,40 +113,14 @@ spa.shell = (function() {
     stateMap.$container = $container;
     $container.html(configMap.main_html);
     setJqueryMap();
+
+    // инициализировать окно чата и привязать обработчик щелчка
+    stateMap.is_chat_retracted = true;
+    jqueryMap.$chat
+      .attr('title', configMap.chat_retracted_title)
+      .on('click', onClickChat);
   };
 
   return { initModule: initModule };
   //----------------- КОНЕЦ ОТКРЫТЫХ МЕТОДОВ ---------------------
 }());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
